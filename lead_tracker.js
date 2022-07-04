ed112710a648ab418ad432e144fcf25cc5acd6ca
jemini-bhut <18ce014.jemini.bhut@vvpedulink.ac.in>
2022-07-04 16:21:58 +0530

    Commit changes

---
+++ lead_tracker.js
@@ -0,0 +1,9 @@
+let myLeads = [];
+const inputEl = document.getElementById("lead-input");
+
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function() {
+    
+})
+
